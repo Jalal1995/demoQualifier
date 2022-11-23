dd7da1310a13fc443008cd47991b1563e441121d
Jalal1995 <ibcelal@gmail.com>
2022-11-23 11:29:28 +0400

    Initial commit

---
+++ src/main/java/com/example/demoqualifier/DemoQualifierApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoqualifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoQualifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoQualifierApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demoqualifier/TestController.java
@@ -0,0 +1,13 @@
+package com.example.demoqualifier;
+
+import com.example.demoqualifier.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private TestService testService;
+
+}
